eea14c711f44d42066356a15a2ac99c17e1c5bec
Edward0475 <edwardjeremyjeremypanjaitan@gmail.com>
2026-06-08 00:52:54 +0700

    Create api_config.dart

---
+++ frontend_app/lib/services/api_config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  // Ganti ke alamat IP Wi-Fi mu jika test pakai HP Asli,
+  // atau gunakan 10.0.2.2 jika pakai Emulator Android.
+  static const String baseUrl = 'http://10.0.2.2:3000';
+}
